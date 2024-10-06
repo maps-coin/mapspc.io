71e75c4b23289a147fedd4dc1ffaf852f1cae0cc
maps-coin <mapspeopleconnection@gmail.com>
2024-10-06 14:14:42 +0400

    added x-twitter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
         </h1>
       </div>
       <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <a href='#telegram' target='_blank'>
+        <a href='https://x.com/Mapsconnection' target='_blank'>
           <FaTelegram />
         </a>
         <a href='#twitter' target='_blank'>
