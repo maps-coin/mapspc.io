6883e0bbc5e468f30d2b53f305e1c053dfa02331
maps-coin <mapspeopleconnection@gmail.com>
2024-10-19 04:27:44 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,14 +13,7 @@ function Footer() {
         </div>
         <div className='flex info_footer items-center'>
           <div className='text-sm text-neutral-500'>
-            Created by{" "}
-            <a
-              className='bg-gradient-to-br from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-transparent tracking-tight'
-              href='https://github.com/emilabbasov06/'
-            >
-              Emil Abbasov
-            </a>{" "}
-            and organized by MAPS.io
+            Attention! It is not an investment advice.
           </div>
           <div className='text-sm text-neutral-500'>
             Enjoy the game. Good Luck!
